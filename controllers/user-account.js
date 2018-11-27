946f22c294b11ec291b26cfbad412fff7a4c28b3
atul_kiwitech <atul.gupta@kiwitech.com>
2018-11-27 17:33:07 +0530

    commenting the code

--- controllers/user-account.js
+++ controllers/user-account.js
@@ -2,8 +2,22 @@ class UserAccountController {
 
     constructor() { }
 
+    /**
+     * @description To registered the new user
+     * @param {*} req
+     * @param {*} res
+     */
     registration(req, res) {
         res.status(200).json({ message: 'Controller calling...' });
+    }
+
+    /**
+     * @description for authorization the user
+     * @param {*} req
+     * @param {*} res
+     */
+    login(req, res) {
+
     }
 }
 let user = new UserAccountController();
